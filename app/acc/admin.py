23ec97362a4d361f0e9d3090ca67008f0fbb5b60
Peter.Fang <fy1716@gmail.com>
2019-02-21 16:58:44 +0800

    init

--- app/acc/admin.py
+++ app/acc/admin.py
@@ -1,3 +1,13 @@
+from django.apps import apps
 from django.contrib import admin
+from .apps import AccConfig
 
 # Register your models here.
+
+# get app name
+name = AccConfig.name.split('.')[-1]
+app = apps.get_app_config(name)
+
+# register model
+for model_name, model in app.models.items():
+    admin.site.register(model)
