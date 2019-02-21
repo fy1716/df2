0a9bb6b19422fb07bf24c1fd92a1046916aea9c7
Peter.Fang <fy1716@gmail.com>
2019-02-21 17:42:54 +0800

    init

--- app/acc/models.py
+++ app/acc/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-
+# 配件
 class AccManage(models.Model):
     station_id = models.IntegerField(null=True, verbose_name="站点ID")
     common_name = models.CharField(max_length=48, null=True, verbose_name="通用名")
@@ -19,7 +19,25 @@ class AccManage(models.Model):
         db_table = 'acc'
 
 
+# 配件扫描
 class AccScanManage(models.Model):
     station_id = models.SmallIntegerField()
     id_number = models.CharField(max_length=50)
     pid = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = 'accScan'
+
+
+# 进货记录
+class AccBuyRecordManage(models.Model):
+    station_id = models.SmallIntegerField()
+    accBuyDate = models.DateField()
+    name = models.CharField(max_length=48, verbose_name="名称")
+    id_number = models.CharField(max_length=30, verbose_name="件号")
+    type = models.CharField(max_length=30, null=True, verbose_name="类型")
+    cost = models.FloatField(null=True, default=0, verbose_name="材料费")
+    count = models.SmallIntegerField(null=True, verbose_name="数量")
+
+    class Meta:
+        db_table = 'accBuyRecord'
--- app/car/models.py
+++ app/car/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 车辆信息
+class CarInfoManage(models.Model):
+    station_id = models.IntegerField()
+    id_number = models.CharField(max_length=50)
+    number = models.CharField(max_length=50, null=True)
+    type = models.CharField(max_length=10, null=True)
+    but_date = models.DateField(null=True)
+    pro_date = models.DateField(null=True)
+    owner = models.CharField(max_length=50, null=True)
+    tel = models.CharField(max_length=15, null=True)
+    remark = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        db_table = 'carInfo'
