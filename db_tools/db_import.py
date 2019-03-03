225d9791d85a08180d16479e8c1de0a241424c5c
fang.yu <909364345@qq.com>
2019-03-04 01:19:55 +0800

    导入数据

--- db_tools/db_import.py
+++ db_tools/db_import.py
@@ -20,11 +20,11 @@ import MySQLdb
 # connect() 方法用于创建数据库的连接，里面可以指定参数：用户名，密码，主机等信息。
 # 这只是连接到了数据库，要想操作数据库需要创建游标。
 conn = MySQLdb.connect(
-    host='119.23.130.227',
+    host='localhost',
     port=3306,
-    user='df',
-    passwd='Yydfsk.com123',
-    db='defeng',
+    user='123',
+    passwd='23',
+    db='db',
     charset='utf8'
 )
 
